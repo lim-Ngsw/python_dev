205d981c0f580baaaa651a538662077576c55de0
lim-Ngsw <ancelotty@gmail.com>
2021-06-13 07:34:01 +0900

    add comment

--- num_method.py
+++ num_method.py
@@ -1,9 +1,9 @@
-import math
+import math #ライブラリをインポート
 
-result = math. sqrt(25)
-print(result)
+result = math. sqrt(25) #平方根
+print(result) #5.0
 
 y = math. log2(10)
-print(y)
+print(y) #3.321928094887362
 
 print(help(math))
--- numbers.py
+++ numbers.py
@@ -1,27 +1,26 @@
-print(2 + 2)
-print(5 - 2)
-print(5 * 6)
-print((50 - 5) * 6)
-print(8 / 5)
-type(1)
-type(1.6)
-print(0.6)
-print(.6)
-print(17 / 3)
-print(17 // 3)
-print(17 % 3)
-print(5 * 5)
-print(5 * 2)
-print(5 * 5 * 5)
-print(5 * 5 * 5 * 5 * 5)
-print(5 ** 5)
+print(2 + 2) #4
+print(5 - 2) #3
+print(5 * 6) #30
+print((50 - 5) * 6) #270
+print(8 / 5) #1.6
+#type(1) <class 'int'>
+#type(1.6) <class 'float'>
+print(0.6)  #0.6
+print(.6) #0.6
+print(17 / 3) #近似値 5.6666666666667
+print(17 // 3) #5
+print(17 % 3) #あまり2
+print(5 * 5) #25
+print(5 ** 2) #25
+print(5 * 5 * 5 * 5 * 5) #3125
+print(5 ** 5) #3125
 
 x = 5
-print(x)
+print(x) #5
 
 y = 10
-print(x * y)
+print(x * y) #50
 
 pie = 3.1415151515
-print(pie)
-print(round(pie, 2))
+print(pie) #3.1415151515...
+print(round(pie, 2)) #3.14（小数点第二位までの丸め）
